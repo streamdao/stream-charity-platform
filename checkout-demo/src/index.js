fa8c9e301dd223b243893b358100b46cfff9a57a
Fyfy Dev <101877327+streamdao@users.noreply.github.com>
2022-03-19 13:45:15 +0200

    First commit

---
+++ checkout-demo/src/index.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import CheckoutDemo from './CheckoutDemo';
+import reportWebVitals from './reportWebVitals';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#0088FE",
+    },
+    secondary: {
+      main: "#5b00b5",
+      dark: "#e7e0ec",
+      light: "#fbf9fd",
+    },
+    success: {
+      main: "#00C49F",
+    },
+    error: {
+      main: "#ff4200",
+    },
+    info: {
+      main: "#FFBB28",
+    },
+  },
+  shape: {
+      borderRadius: 16,
+  },
+  typography: {
+    fontFamily: '"Barlow", "Roboto", "Helvetica", "Arial", sans-serif',
+    h1: {
+      fontWeight: 700,
+    },
+    h2: {
+      fontWeight: 700,
+    },
+    h3: {
+      fontWeight: 700,
+    },
+    h4: {
+      fontWeight: 700,
+    },
+    h5: {
+      fontWeight: 700,
+    },
+    h6: {
+      fontWeight: 600,
+    },
+    body1: {
+      fontWeight: 600,
+    },
+    button: {
+      fontWeight: 700,
+      textTransform: "none",
+    },
+  },
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <CheckoutDemo />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ recipient-portal/src/analytics-page/components/VolumeByDay.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { ResponsiveContainer, LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+
+function VolumeByDay(props) {
+  const { data } = props;
+
+  return (
+    <ResponsiveContainer width="100%" height={280}>
+      <LineChart data={data}>
+        <Line type="monotone" dataKey="value" stroke="#8884d8" />
+        <CartesianGrid stroke="#ccc" />
+        <XAxis dataKey="date" />
+        <YAxis label={{ value: 'Dollars', angle: -90, position: 'insideLeft' }} />
+        <Tooltip />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+}
+
+export default VolumeByDay;
